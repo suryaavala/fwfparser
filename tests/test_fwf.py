902394e9db170d76a3ccbf78013652461041f357
Surya <surya.avala@eliiza.com.au>
2020-01-07 18:03:47 +1100

    added basic tests

--- tests/test_fwf.py
+++ tests/test_fwf.py
@@ -1,12 +1,56 @@
 import pytest
-from fwfparser.fwf import fwf
+from fwfparser.__main__ import main
+from fwfparser.fwf import read_fwf, DataFrameF
+import json
 
 
-def test_no_spec_file_exception():
-    """Test that an exception is raised when no path to spec file is not given
-    """
-    with pytest.raises(TypeError) as error:
-        assert fwf()
-    assert (
-        str(error.value) == "__init__() missing 1 required positional argument: 'spec'"
-    )
+class TestMainPath:
+    def test_no_spec_file_exception_main(self):
+        """Test that an exception is raised when no path to spec file is not given from main()
+        """
+        with pytest.raises(TypeError) as error:
+            assert main()
+        assert (
+            str(error.value) == "main() missing 1 required positional argument: 'spec'"
+        )
+
+
+class TestReadfwf:
+    def test_no_spec_read_fwf(self):
+        """Test that an exception is raised when no path to spec file is not given from read_fwf()
+        """
+        with pytest.raises(TypeError) as error:
+            assert read_fwf()
+        assert (
+            str(error.value)
+            == "read_fwf() missing 1 required positional argument: 'spec'"
+        )
+
+    def test_read_fwf_output_type(self):
+        """Test that read_fwf() returns an instance of class fwf()
+        """
+        assert isinstance(read_fwf(spec="tests/test_spec_valid.json"), DataFrameF)
+
+
+class TestDataFrameF:
+    def test_DataFrameF_struct(self):
+        df = DataFrameF()
+        assert isinstance(df.columns, list)
+        assert isinstance(df.rows, list)
+
+    def test_invalid_spec_file(self):
+        with pytest.raises(ValueError):
+            assert DataFrameF(spec="tests/test_spec_invalid_json.json")
+
+    def test_valid_spec_file(self):
+        assert isinstance(read_fwf(spec="tests/test_spec_valid.json"), DataFrameF)
+
+    def test_not_min_specs(self, tmpdir):
+        with open("tests/test_spec_invalid_minimum.json", "r") as specs_file:
+            specs = json.loads(specs_file.read())
+        for spec_number in specs:
+            path = "test.json"
+            with open(path, "w") as tmp_file:
+                tmp_file.write(json.dumps(specs[spec_number]))
+            with pytest.raises(ValueError):
+                assert DataFrameF(spec=path)
