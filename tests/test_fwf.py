19642f9db6f4e5976a07f1ed5a0970db741c4803
Surya <surya.avala@eliiza.com.au>
2020-01-08 18:03:44 +1100

    more tests

--- tests/test_fwf.py
+++ tests/test_fwf.py
@@ -3,6 +3,12 @@ from fwfparser.__main__ import main
 from fwfparser.fwf import read_fwf, DataFrameF
 import json
 
+# import os
+
+VALID_SPEC_FILE = "tests/test_spec_valid.json"
+VALID_FWF_FILE = "tests/test_fwf_valid.txt"
+VALID_CSV_FILE = "tests/test_csv_valid.csv"
+
 
 class TestMainPath:
     def test_no_spec_file_exception_main(self):
@@ -14,36 +20,56 @@ class TestMainPath:
             str(error.value) == "main() missing 1 required positional argument: 'spec'"
         )
 
+    # def test_main_defaults(self):
+    #     """Test that the package defaults to generating a random fwf file and converting it to csv
+    #     """
+    #     main(spec=VALID_SPEC_FILE)
+    #     files = os.listdir(".")
+    #     assert "sample_fwf.txt" in files
+    #     assert "sample_fwf_parsed.csv" in files
+
 
 class TestReadfwf:
-    def test_no_spec_read_fwf(self):
+    def test_no_specread_fwf(self):
         """Test that an exception is raised when no path to spec file is not given from read_fwf()
         """
         with pytest.raises(TypeError) as error:
             assert read_fwf()
         assert (
             str(error.value)
-            == "read_fwf() missing 1 required positional argument: 'spec'"
+            == "read_fwf() missing 2 required positional arguments: 'spec_path' and 'fwf_path'"
         )
 
-    def test_read_fwf_output_type(self):
+    def testread_fwf_output_type(self):
         """Test that read_fwf() returns an instance of class fwf()
         """
-        assert isinstance(read_fwf(spec="tests/test_spec_valid.json"), DataFrameF)
+        assert isinstance(
+            read_fwf(spec_path=VALID_SPEC_FILE, fwf_path=VALID_FWF_FILE), DataFrameF
+        )
+
+    def test_no_fwf(self):
+        """Test that an exception is raised when no path to spec file is not given from read_fwf()
+        """
+        with pytest.raises(Exception) as error:
+            assert read_fwf(spec_path=VALID_SPEC_FILE, fwf_path="")
+
+        assert "[Errno 2] No such file or directory:" in str(error.value)
 
 
 class TestDataFrameF:
     def test_DataFrameF_struct(self):
         df = DataFrameF()
-        assert isinstance(df.columns, list)
         assert isinstance(df.rows, list)
 
     def test_invalid_spec_file(self):
         with pytest.raises(ValueError):
-            assert DataFrameF(spec="tests/test_spec_invalid_json.json")
+            assert DataFrameF().read_fwf(spec_path="tests/test_spec_invalid_json.json")
 
     def test_valid_spec_file(self):
-        assert isinstance(read_fwf(spec="tests/test_spec_valid.json"), DataFrameF)
+        assert isinstance(
+            DataFrameF().read_fwf(spec_path=VALID_SPEC_FILE, fwf_path=VALID_FWF_FILE),
+            DataFrameF,
+        )
 
     def test_not_min_specs(self, tmpdir):
         with open("tests/test_spec_invalid_minimum.json", "r") as specs_file:
@@ -53,4 +79,17 @@ class TestDataFrameF:
             with open(path, "w") as tmp_file:
                 tmp_file.write(json.dumps(specs[spec_number]))
             with pytest.raises(ValueError):
-                assert DataFrameF(spec=path)
+                assert DataFrameF().read_fwf(spec_path=path)
+
+    def test_parse(self, tmpdir):
+        DataFrameF().read_fwf(
+            spec_path=VALID_SPEC_FILE, fwf_path=VALID_FWF_FILE
+        ).to_csv("test_generated_csv.csv")
+        with open(VALID_CSV_FILE, "r") as v:
+            valid = v.read()
+        with open("test_generated_csv.csv", "r") as g:
+            generated = g.read()
+        assert valid == generated
+
+
+# TODO: Column name is bigger than the offset?
